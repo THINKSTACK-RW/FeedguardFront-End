c0b1d3e913170e3fee5a26d50a82964a213d9ffb
christiannshuti <chrisnshuti943@gmail.com>
2026-05-07 08:39:59 +0200

    Fixing render deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 10000,
+    allowedHosts: ['feedguardfront-end.onrender.com', 'localhost', '0.0.0.0'],
   },
 })
